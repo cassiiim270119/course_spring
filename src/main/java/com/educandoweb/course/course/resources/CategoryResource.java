ce78e360991a52450c1d5b112a070843edbdb828
Cassiano Moura <casse.sousa@gmail.com>
2019-09-21 13:54:15 -0300

    Challange category CRUD

--- src/main/java/com/educandoweb/course/course/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/course/course/resources/CategoryResource.java
@@ -1,15 +1,13 @@
 package com.educandoweb.course.course.resources;
 
 import com.educandoweb.course.course.entities.Category;
-import com.educandoweb.course.course.entities.Order;
 import com.educandoweb.course.course.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +25,25 @@ public class CategoryResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Category> findById(@PathVariable Long id) {
         return ResponseEntity.ok(categoryService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category category) {
+        Category savedCategory = categoryService.insert(category);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedCategory.getId()).toUri();
+        return ResponseEntity.created(uri).body(savedCategory);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id,
+                                       @RequestBody Category category) {
+        return ResponseEntity.ok().body(categoryService.update(id, category));
     }
 }
--- src/main/java/com/educandoweb/course/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/course/services/CategoryService.java
@@ -1,11 +1,15 @@
 package com.educandoweb.course.course.services;
 
 import com.educandoweb.course.course.entities.Category;
-import com.educandoweb.course.course.entities.Order;
 import com.educandoweb.course.course.repositories.CategoryRepository;
+import com.educandoweb.course.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -19,6 +23,31 @@ public class CategoryService {
     }
 
     public Category findById(Long id) {
-        return categoryRepository.findById(id).get();
+        return categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public Category insert(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id) {
+        try {
+            categoryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Category update(Long id, Category category) {
+        try {
+            Category entity = categoryRepository.getOne(id);
+            entity.setName(category.getName());
+            return categoryRepository.save(category);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
 }
